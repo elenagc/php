6ff4b190f4f649106cd305171902b5143ab4a856
elenagc <elenagc84@gmail.com>
2023-11-27 10:50:19 +0100

    Archivos de recogida de datos $_POST[] y $_GET[]

---
+++ recogidaDeDatos/index-saluda.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    Introduce tu nombre:
+    <form action="saluda.php" method="post">
+    <input type="text" name="nombre"><br>
+    <input type="submit" value="Enviar">
+    </form>
+  </body>
+</html>
---
+++ recogidaDeDatos/saluda.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    Hola <?= $_POST['nombre'] ?>,
+    que tengas un buen día.
+  </body>
+</html>
